120b5afb151127de9ec51640d8512575427044c1
kobezzza <kobezzza@gmail.com>
2016-02-21 14:25:10 +0300

    Added gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+'use strict';
+
+/*!
+ * snakeskin.github.io
+ * https://github.com/SnakeskinTpl/snakeskin.github.io
+ *
+ * Released under the MIT license
+ * https://github.com/SnakeskinTpl/snakeskin.github.io/blob/master/LICENSE
+ */
+
+const
+	gulp = require('gulp'),
+	snakeskin = require('gulp-snakeskin'),
+	stylus = require('gulp-stylus'),
+	autoprefixer = require('gulp-autoprefixer'),
+	watch = require('gulp-watch');
+
+function error(cb) {
+	return (err) => {
+		console.error(err.message);
+		cb();
+	};
+}
+
+gulp.task('snakeskin', (cb) => {
+	gulp.src('./index.ss')
+		.pipe(watch('./**/*.ss'))
+		.pipe(snakeskin({exec: true, prettyPrint: true}))
+		.on('error', error(cb))
+		.pipe(gulp.dest('./build'))
+		.on('end', cb);
+});
+
+gulp.task('stylus', (cb) => {
+	gulp.src('./styles/index.styl')
+		.pipe(watch('./styles/**/*.styl'))
+		.pipe(stylus())
+		.on('error', error(cb))
+		.pipe(autoprefixer())
+		.pipe(gulp.dest('./build/css'))
+		.on('end', cb);
+});
+
+gulp.task('default', ['snakeskin', 'stylus']);
