a0e9941db2ce6468105b4b4fb3cd13a625d78055
kobezzza <kobezzza@gmail.com>
2016-03-13 17:16:35 +0300

    Refactoring

--- scripts/index.js
+++ scripts/index.js
@@ -8,18 +8,5 @@
  * https://github.com/SnakeskinTpl/snakeskin.github.io/blob/master/LICENSE
  */
 
-$(() => {
-	$(document).on('click', '.b-example__link:not(.b-example__link_active_true)', (e) => {
-		const
-			el = $(e.target),
-			block = el.closest('.b-example'),
-			val = el.text();
-
-		el
-			.addClass('b-example__link_active_true')
-			.siblings().removeClass('b-example__link_active_true');
-
-		block.find(`.b-example__code > pre:not(.example-${val})`).hide();
-		block.find(`.example-${val}`).show();
-	});
-});
+import './b-example';
+import './navigation';
